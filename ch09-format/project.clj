4e4b06bd331889a23a1b2947b9cda462bf3f68a6
rz <muhamaul@gmail.com>
2023-02-20 11:43:34 +0700

    finished chapter 9

--- ch09-format/project.clj
+++ ch09-format/project.clj
@@ -3,7 +3,10 @@
   :url "http://example.com/FIXME"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
-  :dependencies [[org.clojure/clojure "1.11.1"]]
+  :dependencies [[org.clojure/clojure "1.11.1"]
+                 [org.clojure/data.json "2.4.0"]
+                 [cheshire "5.11.0"]
+                 [com.cognitect/transit-clj "1.0.329"]]
   :main ^:skip-aot ch09-format.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all
--- ch09-format/src/ch09_format/core.clj
+++ ch09-format/src/ch09_format/core.clj
@@ -1,5 +1,10 @@
 (ns ch09-format.core
-  (:require [clojure.edn :as edn])
+  (:require [clojure.edn :as edn]
+            [clojure.data.json :as j]
+            [cheshire.core :refer :all]
+            [cheshire.generate :refer [add-encoder]]
+            [cognitect.transit :as transit])
+  (:import [java.io ByteArrayInputStream ByteArrayOutputStream])
   (:gen-class))
 
 ;; edn
@@ -49,3 +54,118 @@
 (defn write-text-to-file [text f]
   (with-open [w (clojure.java.io/writer f)]
     (.write w text)))
+
+;; json
+
+;; using data.json
+(j/read-str "[{\"name\" : \"Ben\", \"age\" :39}, {\"name\": \"Monster\", \"age\": 4}]")
+;; => [{"name" "Ben", "age" 39} {"name" "Monster", "age" 4}]
+
+(j/write-str [{:name "Riz"}, {:name "Mina" :age 3}])
+;; => "[{\"name\":\"Riz\"},{\"name\":\"Mina\",\"age\":3}]"
+
+;; using cheshire
+(parse-string "[{\"name\" : \"Ben\", \"age\" :39}, {\"name\": \"Monster\", \"age\": 4}]")
+;; => ({"name" "Ben", "age" 39} {"name" "Monster", "age" 4})
+
+(generate-string [{:name "Riz"}, {:name "Mina" :age 3}])
+;; => "[{\"name\":\"Riz\"},{\"name\":\"Mina\",\"age\":3}]"
+
+(parse-string "[{\"name\" : \"Ben\", \"age\" :39}, {\"name\": \"Monster\", \"age\": 4}]" true)
+;; => ({:name "Ben", :age 39} {:name "Monster", :age 4})
+
+(generate-string {:date (java.util.Date.)})
+;; => "{\"date\":\"2023-02-20T03:45:56Z\"}"
+
+(def ^:private date-format
+  (proxy [ThreadLocal] []
+    (initialValue []
+      (doto (java.text.SimpleDateFormat. "yyyy-MM-dd'T'HH:mm:ss.SSSXXX")))))
+
+(defn- format-inst
+  "create a tolerable string from an inst"
+  [d]
+  (str "#inst (.format (.get date-format) d)"))
+
+(defn- date-part
+  "extract the date part of a stringified #inst"
+  [d]
+  (second (re-matches #"#inst (.*)" d)))
+
+(add-encoder java.util.Date
+             (fn [d generator]
+               (.writeString generator (format-inst d))))
+
+(generate-string {:date (java.util.Date.)})
+;; => "{\"date\":\"#inst (.format (.get date-format) d)\"}"
+
+(defn read-date [k v]
+  (if (= :date k)
+    (.parse (.get date-format) (date-part v))
+    v))
+
+(defn write-date [k v]
+  (if (= :date k)
+    (str "#inst " (.format (.get date-format) v))
+    v))
+
+(j/write-str {:keyword "string"
+              :date (java.util.Date.)}
+              :value-fn write-date)
+;; => "{\"keyword\":\"string\",\"date\":\"#inst 2023-02-20T11:11:24.843+07:00\"}"
+
+(j/read-str
+ (j/write-str {:keyword "string"
+               :date (java.util.Date.)}
+              :value-fn write-date)
+ :key-fn keyword
+ :value-fn read-date)
+;; => {:keyword "string", :date #inst "2023-02-20T04:12:18.546-00:00"}
+
+;;; transit
+(def output (ByteArrayOutputStream. 4096))
+(def writer (transit/writer output :json-verbose))
+
+(transit/write writer [
+                       {:name "joe smith"
+                        :email "joe@company.com"
+                        :roles [:admin :supervisor :analyst]}
+                       {:name "robert jones"
+                        :email "rob@company.com"
+                        :roles [:analyst]}
+                       ])
+;; => nil
+
+(declare validate-same-currency)
+
+(defrecord Currency [divisor sym desc])
+
+(defrecord Money [amount ^Currency currency]
+  java.lang.Comparable
+  (compareTo [m1 m2]
+    (validate-same-currency m1 m2)
+    (compare (:amount m1) (:amount m2))))
+
+(def currencies {:usd (->Currency 100 "USD" "US Dollars")
+                 :eur (->Currency 100 "EUR" "Euro")})
+
+(defn- validate-same-currency
+  [m1 m2]
+  (or (= (:currency m1) (:currency m2))
+      (throw
+       (ex-info "Currencies do not match."
+                {:m1 m1 :m2 m2}))))
+
+(def write-handlers {
+                     Currency
+                     (reify WriteHandler
+                       (tag [_ _] "currency")
+                       (rep [_ c] [(:divisor c) (:sym c) (:desc c)])
+                       (stringRep [_ _] nil)
+                       (getVerboseHandler [_] nil))
+                     Money
+                     (reify WriteHandler
+                       (tag [_ m] "money")
+                       (rep [_ m] [(:amount m) (:currency m)])
+                       (stringRep [_ _] nil)
+                       (getVerboseHandler [_] nil))})
